22f298cd6a68326bb45f028c2bc1782e8b5d0a79
Rahul <prajapat09rahul@gmail.com>
2024-10-01 15:50:31 +0530

    Update build.gradle.kts

--- kotlinpermissions-coroutines/build.gradle.kts
+++ kotlinpermissions-coroutines/build.gradle.kts
@@ -51,7 +51,7 @@ publishing{
         register<MavenPublication>("release"){
             groupId = "com.github.com.github.rjmngr929" 
             artifactId = "KotlinPermission"
-            version = "1.0.1"
+            version = "1.1"
 
             afterEvaluate {
                 from(components["release"])
